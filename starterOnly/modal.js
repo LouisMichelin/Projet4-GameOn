ffa7328e1ea6ad751b6f4718603ca185d277003a
LouisMichelin <louis.michelin097@gmail.com>
2023-02-11 22:47:51 +0100

    #2 creation des REGEX pour chaque formulaire

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -37,8 +37,6 @@ function closeModal() {
 //
 // #2 Implémenter les entrées du formulaire
 //
-//            CHOSES A FAIRE :
-//
 // Le formulaire doit être valide quand l'utilisateur clique sur "Submit"
 // Les données doivent être saisies correctement :
 //  (1) Le champ Prénom a un minimum de 2 caractères / n'est pas vide.
@@ -47,9 +45,7 @@ function closeModal() {
 //  (4) Pour le nombre de concours, une valeur numérique est saisie.
 //  (5) Un bouton radio est sélectionné.
 //  (6) La case des conditions générales est cochée, l'autre case est facultative / peut être laissée décochée.
-//
 // Conserver les données du formulaire (ne pas effacer le formulaire) lorsqu'il ne passe pas la validation.
-
 
 // DOM Elements pour formulaire
 const formDataPrenom = document.querySelector("#first");
@@ -60,15 +56,24 @@ const formDataTournois = document.querySelector("#quantity");
 // const formDataLocation =document.querySelector();
 // const formDataCheckbox = document.querySelector();
 
-
-// Vérification conformité Prénom + Nom
-//   REGEX pour toute sorte de lettres: \p{L}
+// Vérification conformité Prénom + Nom 
+let nameRegExp = new RegExp(
+  '^[A-Za-z-]{2,30}$'
+);
 
 // Vérification conformité Adresse e-mail
-//   REGEX "RFC 5322 official standard": ([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|"([]!#-[^-~ \t]|(\\[\t -~]))+")@([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\[[\t -Z^-~]*])
+  // ? REGEX "RFC 5322 official standard": ([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|"([]!#-[^-~ \t]|(\\[\t -~]))+")@([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\[[\t -Z^-~]*])
+let emailRegExp = new RegExp(
+  '^[A-Za-z0-9.-_]+[@]{1}[A-Za-z0-9.-_]+[.]{1}[a-z]{2,10}$'
+);
 
 // Vérification conformité Date de naissance
-//   REGEX pour date de naissance: /^(?:0[1-9]|[12]\d|3[01])([\/.-])(?:0[1-9]|1[012])\1(?:19|20)\d\d$/
+  // ? REGEX pour date de naissance: /^(?:0[1-9]|[12]\d|3[01])([\/.-])(?:0[1-9]|1[012])\1(?:19|20)\d\d$/
+let birthdateRegExp = new RegExp(
+  '^[0-9]{1,2}/[0-9]{1,2}/[1-2]{1}[0-9]{3}$'
+);
 
 // Vérification conformité Nombre de tournois GameOn
-//   REGEX pour nombre de tournois: /^\d{1,3}/
+let tournamentsRegExp = new RegExp(
+  '[0-9]{1,3}'
+);
