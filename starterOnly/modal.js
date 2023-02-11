f9a8eeeae2b6dcbc0cd6958a4b607a8cc88af8a6
LouisMichelin <louis.michelin097@gmail.com>
2023-02-11 19:16:20 +0100

    #2 creation des constantes pour REGEX

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -20,21 +20,55 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-
 //
 // #1 Fermer la modale
 //
+// DOM Element pour fermeture de la modale
 const closeBtn = document.querySelectorAll(".close");
 
-// close modal event
+// Close modal event
 closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
 
-// close modal form
+// Close modal form
 function closeModal() {
   modalbg.style.display = "none";
 }
 
-
 //
 // #2 Implémenter les entrées du formulaire
 //
+//            CHOSES A FAIRE :
+//
+// Le formulaire doit être valide quand l'utilisateur clique sur "Submit"
+// Les données doivent être saisies correctement :
+//  (1) Le champ Prénom a un minimum de 2 caractères / n'est pas vide.
+//  (2) Le champ du nom de famille a un minimum de 2 caractères / n'est pas vide.
+//  (3) L'adresse électronique est valide.
+//  (4) Pour le nombre de concours, une valeur numérique est saisie.
+//  (5) Un bouton radio est sélectionné.
+//  (6) La case des conditions générales est cochée, l'autre case est facultative / peut être laissée décochée.
+//
+// Conserver les données du formulaire (ne pas effacer le formulaire) lorsqu'il ne passe pas la validation.
+
+
+// DOM Elements pour formulaire
+const formDataPrenom = document.querySelector("#first");
+const formDataNom = document.querySelector("#last");
+const formDataEmail = document.querySelector("#email");
+const formDataBirthdate = document.querySelector("#birthdate");
+const formDataTournois = document.querySelector("#quantity");
+// const formDataLocation =document.querySelector();
+// const formDataCheckbox = document.querySelector();
+
+
+// Vérification conformité Prénom + Nom
+//   REGEX pour toute sorte de lettres: \p{L}
+
+// Vérification conformité Adresse e-mail
+//   REGEX "RFC 5322 official standard": ([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|"([]!#-[^-~ \t]|(\\[\t -~]))+")@([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\[[\t -Z^-~]*])
+
+// Vérification conformité Date de naissance
+//   REGEX pour date de naissance: /^(?:0[1-9]|[12]\d|3[01])([\/.-])(?:0[1-9]|1[012])\1(?:19|20)\d\d$/
+
+// Vérification conformité Nombre de tournois GameOn
+//   REGEX pour nombre de tournois: /^\d{1,3}/
